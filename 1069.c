f23986c44842a39c5375665cc206969e9655f299
mummhy0811 <76941500+mummhy0811@users.noreply.github.com>
2021-09-30 00:39:35 +0900

    Create 1069.c

---
+++ 1069.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void) {
+	char a;
+	scanf("%c", &a);
+	if (a == 'A') printf("best!!!");
+	else if (a == 'B') printf("good!!");
+	else if (a == 'C') printf("run!");
+	else if (a == 'D') printf("slowly~");
+	else printf("what?");
+	return 0; 
+}
